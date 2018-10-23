e36d04d1067436203b0698fc1ded237773b20459
jamesot <stephineratemo@gmail.com>
2018-10-23 08:15:52 +0300

    nav init

--- app/src/main/java/com/oneshoppoint/yates/yates/postFeedback.java
+++ app/src/main/java/com/oneshoppoint/yates/yates/postFeedback.java
@@ -1,12 +1,18 @@
 package com.oneshoppoint.yates.yates;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +29,7 @@ import static com.oneshoppoint.yates.yates.utils.Utils.applyFontForToolbarTitle;
 import static com.oneshoppoint.yates.yates.utils.Utils.baseURL;
 import static com.oneshoppoint.yates.yates.utils.Utils.showToast;
 
-public class postFeedback extends AppCompatActivity {
+public class postFeedback extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private EditText name, email, phone, subject, area, feedback, feedback_date;
     protected Typeface mTfLight;
@@ -77,8 +83,25 @@ public class postFeedback extends AppCompatActivity {
                 uploadData();
             }
         });
-    }
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     private void uploadData() {
         Map<String, String> params = new HashMap<String, String>();
@@ -105,5 +128,34 @@ public class postFeedback extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+       /* if (id == R.id.home) {
+            // Handle the camera action
+            Intent intent = new Intent(getBaseContext(), HomePage.class);
+            startActivity(intent);
+        } else if (id == R.id.acceptance) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.build) {
+            Intent intent = new Intent(getBaseContext(), BuildUp.class);
+            startActivity(intent);
+
+        } else if (id == R.id.dispatch) {
+            Intent intent = new Intent(getBaseContext(), Dispatch.class);
+            startActivity(intent);
+
+        } else if (id == R.id.post) {
+            setURL();
+
+        }*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
